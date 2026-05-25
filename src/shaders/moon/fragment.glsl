2bb25231bb6fd2277462bdcd1776b534a8603c79
nullsploit01 <harshaldharmik099@gmail.com>
2026-05-25 22:14:14 +0530

    add noise to add texture on moon

--- src/shaders/moon/fragment.glsl
+++ src/shaders/moon/fragment.glsl
@@ -1,6 +1,12 @@
 uniform float phase;
 uniform vec3 moonColor;
 
+float random(vec2 st) {
+    return fract(
+        sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123
+    );
+}
+
 void main() {
 
     vec2 uv = gl_PointCoord * 2.0 - 1.0;
@@ -17,6 +23,9 @@ void main() {
 
     // diffuse lighting
     float diffuse = max(dot(normal, lightDir), 0.0);  
+    float sphereShade = normal.z * 0.15 + 0.85;
+    diffuse *= sphereShade;
+
     float ambient = (1.0 - abs(phase)) * 0.02;
     diffuse = max(diffuse, ambient);
 
@@ -31,5 +40,17 @@ void main() {
     vec3 baseColor = mix(vec3(0.82), moonColor, 0.25);
 
     vec3 color = baseColor * diffuse + vec3(1.0) * glow;
+    
+    // noise
+    float noise1 = random(floor(uv * 25.0));
+    float noise2 = random(floor(uv * 8.0));
+
+    float noise = mix(
+        noise1,
+        noise2,
+        0.3
+    );
+
+    color *= 1.0 + noise * diffuse * 0.03;
     gl_FragColor = vec4(color, 1.0);
 }
